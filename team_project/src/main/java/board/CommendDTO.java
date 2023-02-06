15eb3ae63f54fd7c6fd55134f31cc9c5f5759e81
user <user@DESKTOP-2PGRMF4>
2023-02-06 12:23:01 +0900

    commendto, replydto update

--- team_project/src/main/java/board/CommendDTO.java
+++ team_project/src/main/java/board/CommendDTO.java
@@ -1,5 +1,50 @@
 package board;
 
-public class CommendDTO {
+import java.sql.Timestamp;
 
+public class CommendDTO {
+	private int cno;
+	private int bno;
+	private int no;
+	private int rno;
+	private String commend;
+	private Timestamp cdate;
+	public int getCno() {
+		return cno;
+	}
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getCommend() {
+		return commend;
+	}
+	public void setCommend(String commend) {
+		this.commend = commend;
+	}
+	public Timestamp getCdate() {
+		return cdate;
+	}
+	public void setCdate(Timestamp cdate) {
+		this.cdate = cdate;
+	}
+	
+	
 }
--- team_project/src/main/java/board/ReplyDTO.java
+++ team_project/src/main/java/board/ReplyDTO.java
@@ -1,5 +1,43 @@
 package board;
 
-public class ReplyDTO {
+import java.sql.Timestamp;
 
+public class ReplyDTO {
+   private int rno;
+   private int no;
+   private int bno;
+   private String riply;
+   private Timestamp rdate;
+   
+   public int getRno() {
+	   return rno;
+   }
+   public void setRno(int rno) {
+	   this.rno = rno;
+   }
+   public int getNo() {
+	   return no;
+   }
+   public void setNo(int no) {
+	   this.no = no;
+   }
+   public int getBno() {
+	   return bno;
+   }
+   public void setBno(int bno) {
+	   this.bno = bno;
+   }
+   public String getRiply() {
+	   return riply;
+   }
+   public void setRiply(String riply) {
+	   this.riply = riply;
+   }
+   public Timestamp getRdate() {
+	   return rdate;
+   }
+   public void setRdate(Timestamp rdate) {
+	   this.rdate = rdate;
+   }
+  
 }
