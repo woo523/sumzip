09f742e2d9ab13003f07e8c995e9209ea191fdb7
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-06 12:10:40 +0900

    webapp folder setting

--- team_project/src/main/java/products/AppointmentDTO.java
+++ team_project/src/main/java/products/AppointmentDTO.java
@@ -1,5 +1,15 @@
 package products;
 
-public class AppointmentDTO {
+import java.sql.Date;
+import java.sql.Timestamp;
 
+public class AppointmentDTO {
+	private int sno;
+	private int ano;
+	private int pno;
+	private int no2;
+	private Timestamp sdate;
+	private Date indate;
+	private Date outdate;
+	private int sprice;
 }
--- team_project/src/main/java/products/WaitingDTO.java
+++ team_project/src/main/java/products/WaitingDTO.java
@@ -1,5 +1,36 @@
 package products;
 
-public class WaitingDTO {
+import java.sql.Timestamp;
 
+public class WaitingDTO {
+	private int wno;
+	private int pno;
+	private int no2;
+	private Timestamp wdate;
+	public int getWno() {
+		return wno;
+	}
+	public void setWno(int wno) {
+		this.wno = wno;
+	}
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public int getNo2() {
+		return no2;
+	}
+	public void setNo2(int no2) {
+		this.no2 = no2;
+	}
+	public Timestamp getWdate() {
+		return wdate;
+	}
+	public void setWdate(Timestamp wdate) {
+		this.wdate = wdate;
+	}
+	
+	
 }
