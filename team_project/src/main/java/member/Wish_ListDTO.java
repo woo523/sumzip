29351043036e9b4d819c374bb98e2d8536a00dfe
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-06 13:44:20 +0900

    update

--- team_project/src/main/java/member/Wish_ListDTO.java
+++ team_project/src/main/java/member/Wish_ListDTO.java
@@ -1,5 +1,18 @@
 package member;
 
 public class Wish_ListDTO {
-
+	private int no;
+	private int pno;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
 }
--- team_project/src/main/java/products/SalesDTO.java
+++ team_project/src/main/java/products/SalesDTO.java
@@ -1,5 +1,71 @@
 package products;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class SalesDTO {
-
+	private int sno;
+	private int ano;
+	private int pno;
+	private int no2;
+	private Timestamp sdate;
+	private Date indate;
+	private Date outdate;
+	private int sprice;
+	private int scount;
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public int getNo2() {
+		return no2;
+	}
+	public void setNo2(int no2) {
+		this.no2 = no2;
+	}
+	public Timestamp getSdate() {
+		return sdate;
+	}
+	public void setSdate(Timestamp sdate) {
+		this.sdate = sdate;
+	}
+	public Date getIndate() {
+		return indate;
+	}
+	public void setIndate(Date indate) {
+		this.indate = indate;
+	}
+	public Date getOutdate() {
+		return outdate;
+	}
+	public void setOutdate(Date outdate) {
+		this.outdate = outdate;
+	}
+	public int getSprice() {
+		return sprice;
+	}
+	public void setSprice(int sprice) {
+		this.sprice = sprice;
+	}
+	public int getScount() {
+		return scount;
+	}
+	public void setScount(int scount) {
+		this.scount = scount;
+	}
+	
 }
