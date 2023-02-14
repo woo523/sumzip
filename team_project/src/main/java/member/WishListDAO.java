c9ae0bbcdcb7827c2280ad5d6489e61f1c82f9dc
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-14 17:32:00 +0900

    wishlist update

--- team_project/src/main/java/member/WishListDAO.java
+++ team_project/src/main/java/member/WishListDAO.java
@@ -7,7 +7,10 @@ import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
+
+import products.ProductDTO;
 
 public class WishListDAO {
 	public Connection getConnection() throws Exception {
@@ -59,25 +62,58 @@ public void deleteWish(WishListDTO dto) {
 	}
 	return;
 	}	
+//
+//public ArrayList<WishListDTO> getWishArrayList(int pno) {
+//	ArrayList<WishListDTO> wishArrayList = new ArrayList<WishListDTO>();
+//	Connection con = null;
+//	PreparedStatement pstmt = null;
+//	ResultSet rs = null;
+//	try {
+//		con = getConnection();
+//		String sql = "select * from Wish_List where id=?";
+//		pstmt = con.prepareStatement(sql);
+//		pstmt.setInt(1, pno);
+//
+//		rs = pstmt.executeQuery();
+//
+//		if (rs.next()) {
+//			WishListDTO dto = new WishListDTO();
+//			dto.setNo(rs.getInt("no"));
+//			dto.setPno(rs.getInt("pno"));
+//			wishArrayList.add(dto);
+//			
+//		}
+//	} catch (Exception e) {
+//		e.printStackTrace();
+//	} finally {
+//		if (pstmt != null) try {pstmt.close();} catch (Exception e2) {}
+//		if (con != null) try {con.close();} catch (Exception e2) {}
+//		if (rs != null) try {rs.close();} catch (Exception e2) {}
+//	}
+//	return wishArrayList;
+//}
 
-public ArrayList<WishListDTO> getWishArrayList(int pno) {
-	ArrayList<WishListDTO> wishArrayList = new ArrayList<WishListDTO>();
+
+public ArrayList<ProductDTO> getWishArrayList(int pno,int no) {
+	ArrayList<ProductDTO> list = new ArrayList<ProductDTO>();
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 	try {
 		con = getConnection();
-		String sql = "select * from Wish_List where id=?";
+		String sql = "select * from Wish_List where pno=? and no=?";
 		pstmt = con.prepareStatement(sql);
 		pstmt.setInt(1, pno);
-
+		pstmt.setInt(2, no);
 		rs = pstmt.executeQuery();
 
 		if (rs.next()) {
-			WishListDTO dto = new WishListDTO();
-			dto.setNo(rs.getInt("no"));
-			dto.setPno(rs.getInt("pno"));
-			wishArrayList.add(dto);
+			ProductDTO dto = new ProductDTO();
+			dto.setPname(rs.getString("Pname"));
+			dto.setPprice(rs.getInt("Pprice"));
+			dto.setPaddress(rs.getString("Paddress"));
+			dto.setPpic1(rs.getString("Ppic1"));
+			list.add(dto);
 			
 		}
 	} catch (Exception e) {
@@ -87,6 +123,6 @@ public ArrayList<WishListDTO> getWishArrayList(int pno) {
 		if (con != null) try {con.close();} catch (Exception e2) {}
 		if (rs != null) try {rs.close();} catch (Exception e2) {}
 	}
-	return wishArrayList;
+	return list;
 }
 }
--- team_project/src/main/webapp/wish_List/Wish_List.jsp
+++ team_project/src/main/webapp/wish_List/Wish_List.jsp
@@ -1,38 +1,50 @@
+<%@page import="products.ProductDTO"%>
 <%@page import="member.UserDTO"%>
 <%@page import="member.UserDAO"%>
 <%@page import="member.WishListDAO"%>
-<%@page import="member.WishListDTO"%>
 <%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
-
+    
+<!-- 헤더파일들어가는 곳 -->
+<jsp:include page="../inc/header.jsp" />
+<!-- 헤더파일들어가는 곳 -->
+<h1>위시리스트</h1>
 <%
 String id=(String)session.getAttribute("id");
+if(id!=null){
+	UserDAO udao=new UserDAO();
+	UserDTO dto=udao.getUser(id); //no
+	WishListDAO dao= new WishListDAO();
+	int no=dto.getNo();
+	ArrayList<ProductDTO> list=new ArrayList<ProductDTO>();
+	int pno=3;
 
-UserDAO udao = new UserDAO();
-UserDTO udto = udao.getUser(id); //no
+	%>
 
-int pno=3;
-int no=udto.getNo();
-%>
-
-
-<table border=1>
+	<table border=1>
+	<tr><td>유저이름</td><td>숙박가격</td><td>주소</td><td>ppic1</td></tr>
 	<%
-		
-		WishListDAO dao= new WishListDAO();
-		ArrayList<WishListDTO> list = dao.getWishArrayList(pno);
-		for(int i=0; i<list.size(); i++){	
+		list = dao.getWishArrayList(pno,no);
+		for(int i=0; i<list.size(); i++){
+		ProductDTO d=list.get(i);
 	%>
 	<tr>
-		<td><%= list.get(i).getNo() %></td>
-		<td><%= list.get(i).getPno() %></td>
-		
+		<td><%=d.getPname()%></td>
+		<td><%=d.getPprice()%></td>
+		<td><%=d.getPaddress()%></td>
+		<td><%=d.getPpic1()%></td>
 	</tr>
 	<%
 		}
 	%>
 </table>
-
-<!-- 넘어온 값이 on이면 insertWish, off면 deletewish -->
+<a href="../member/myPage_user.jsp">마이페이지로 돌아가기</a><br>
+<%
+}else {
+	response.sendRedirect("../member/login.jsp");
+}
+%>
+<!-- 푸터 들어가는 곳 -->
+<jsp:include page="../inc/footer.jsp" />
+<!-- 푸터 들어가는 곳 -->
