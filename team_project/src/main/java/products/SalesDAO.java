5624514ffdaa8d0520f473fe77aa585cb1e06f4d
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-20 16:05:10 +0900

    DAO 주석 수정 & 삭제

--- team_project/src/main/java/products/SalesDAO.java
+++ team_project/src/main/java/products/SalesDAO.java
@@ -19,31 +19,21 @@ public class SalesDAO {
 		Connection con=ds.getConnection();
 		return con;
 	}
-		
+	
+	// 날짜 기준으로 회원별 sales테이블 조회
 	public SalesDTO getSales(int no) {
 		SalesDTO dto=null;
 		Connection con=null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		try {
-			//1,2 디비연결 메서드
 			con=getConnection();
-			
-			//3단계 SQL구문 만들어서 실행할 준비(select 조건 where id=?)
 			String sql="select * from sales where no=? order by sdate desc";
 			pstmt=con.prepareStatement(sql);
 			pstmt.setInt(1, no);
-
-			//4단계 SQL구문을 실행(select) => 결과 저장
 			rs=pstmt.executeQuery();
-			//5단계 결과를 출력, 데이터 담기 (select)
-			// next() 다음행 => 리턴값 데이터 있으면 true/ 데이터 없으면 false
-			//조건이 true 실행문=> 다음행 데이터 있으면 true =>  열접근 출력
 			if(rs.next()){
-				//next() 다음행 => 리턴값 데이터 있으면 true/ 아이디 일치
-				// 바구니 객체생성 => 기억장소 할당
 				dto=new SalesDTO();
-				// set메서드호출 바구니에 디비에서 가져온 값 저장
 				dto.setSno(rs.getInt("sno"));
 				dto.setAno(rs.getInt("ano"));
 				dto.setPno(rs.getInt("pno"));
@@ -56,7 +46,6 @@ public class SalesDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
-			// 예외 상관없이 마무리작업 => 객체생성한 기억장소 해제
 			if(rs!=null) try { rs.close();} catch (Exception e2) {}
 			if(pstmt!=null) try { pstmt.close();} catch (Exception e2) {}
 			if(con!=null) try { con.close();} catch (Exception e2) {}
@@ -64,25 +53,19 @@ public class SalesDAO {
 		return dto;
 	}//getSales()
 	
+	// sales 테이블 조회
 	public ArrayList<SalesDTO> getSalesList(){
 		ArrayList<SalesDTO> SalesList=new ArrayList<SalesDTO>();
 		Connection con =null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		try {
-			//1,2 디비연결 메서드
 			con=getConnection();
-			//3단계 SQL구문 만들어서 실행할 준비(select)
 			String sql="select * from Sales";
 			pstmt=con.prepareStatement(sql);
-			//4단계 SQL구문을 실행(select) => 결과 저장
 			rs=pstmt.executeQuery();	
-			//5단계	//조건이 true 실행문=> 다음행 데이터 있으면 true 
-			//     =>  열접근 => 한 명 정보 SalesDTO 저장 => 배열한칸 저장 
 			while(rs.next()) {
-				// SalesDTO 객체생성
 				SalesDTO dto=new SalesDTO();
-				// set메서드 호출 <= 열접근
 				dto.setSno(rs.getInt("sno"));
 				dto.setAno(rs.getInt("ano"));
 				dto.setPno(rs.getInt("pno"));
@@ -91,13 +74,11 @@ public class SalesDAO {
 				dto.setOutdate(rs.getDate("outdate"));
 				dto.setSprice(rs.getInt("sprice"));
 				dto.setScount(rs.getInt("scount"));
-				// 배열 한칸에 회원정보주소 저장
 				SalesList.add(dto);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
-			// 예외 상관없이 마무리작업 => 객체생성한 기억장소 해제
 			if(rs!=null) try { rs.close();} catch (Exception e2) {}
 			if(pstmt!=null) try { pstmt.close();} catch (Exception e2) {}
 			if(con!=null) try { con.close();} catch (Exception e2) {}
