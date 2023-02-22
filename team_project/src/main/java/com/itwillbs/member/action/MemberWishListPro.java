b00219add536841e75c0383aed8538504a234f19
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-22 15:49:25 +0900

    wishlist, product content

--- team_project/src/main/java/com/itwillbs/member/action/MemberWishListPro.java
+++ team_project/src/main/java/com/itwillbs/member/action/MemberWishListPro.java
@@ -27,8 +27,7 @@ int no=dto.getNo();
 WishListDAO dao= new WishListDAO();
 int pno=Integer.parseInt(request.getParameter("pno"));
 System.out.println("pno="+pno);
-boolean result=true;
-PrintWriter out=response.getWriter();
+
 ActionForward forward=null;
 response.setCharacterEncoding("UTF-8");
 response.setContentType("text/html; charset=UTF-8");
@@ -36,38 +35,44 @@ response.setContentType("text/html; charset=UTF-8");
 
 		if(id!=null){ //로그인 되어있을때
 			if ("찜".equals(order)){
-			result=dao.WishCheck(no,pno);
+			boolean result=dao.WishCheck(no,pno);
 			System.out.println("result="+result);
-			if (result==false){ //DB에 숙소가 안담겨있을때
-				dao.insertWish(no,pno);
-				System.out.println("wishcheck");
-				out.println("<script type='text/javascript'>");
-				out.println("alert('해당항목을 찜 목록에 담았습니다.');");
-				out.println("history.back();");
-				out.println("</script>");
-				forward = new ActionForward();
-				forward.setPath("MemberWishList.me");//나중에 버튼을 눌렀던 그 페이지 주소로 바꿔놓기
-				forward.setRedirect(true);
-				}else if (result==true){
-					System.out.println("wishcheck2");
+				if (result==false){ //DB에 숙소가 안담겨있을때
+					dao.insertWish(no,pno);
+					response.setContentType("text/html; charset=UTF-8");
+					PrintWriter out=response.getWriter();
+					System.out.println("wishcheck");
 					out.println("<script type='text/javascript'>");
-					out.println("alert('이미 담긴 항목입니다.');");
+					out.println("alert('해당항목을 찜 목록에 담았습니다.');");
+					out.println("history.back();");
 					out.println("</script>");
-				
-					}
+					}else if (result==true){
+						response.setContentType("text/html; charset=UTF-8");
+						PrintWriter out=response.getWriter();
+						System.out.println("wishcheck2");
+						out.println("<script type='text/javascript'>");
+						out.println("alert('이미 담긴 항목입니다.');");
+						out.println("history.back();");
+						out.println("</script>");
+					
+						}
 			}
 			else if("찜해제".equals(order)){
+				System.out.println("찜해제");
 				dao.deleteWish(no, pno);
 				response.sendRedirect("MemberWishList.me");
 				}
 				
-				else { //로그인 안되어있을때
-					forward = new ActionForward();
-				 	forward.setPath("MemberLogin.me");
-				 	forward.setRedirect(true);
-		
+				
 	}
+		else { //로그인 안되어있을때
+			System.out.println("로그인안됨");
+			forward = new ActionForward();
+		 	forward.setPath("MemberLogin.me");
+		 	forward.setRedirect(true);
+		 	return forward;
 		}
 		return null;
-		}}
+		}
 
+}
